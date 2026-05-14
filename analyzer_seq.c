f9432829bd9157d746357bfa34d47c14e364855a
M4t3u5534 <mateusrcerqueira@gmail.com>
2026-05-14 08:54:56 -0300

    funcao

--- analyzer_seq.c
+++ analyzer_seq.c
@@ -5,11 +5,31 @@
 
 #include "hash_table.h"
 
-int main() {
+void ler_log(HashTable* ht, FILE* file) {
+    char line[1024];
+
+    while (fgets(line, sizeof(line), file)) {
+        char *ptr = line; // Ponteiro para percorrer a linha
+        char *urlStart = NULL; // Ponteiro para marcar o início da URL
+        while (*ptr != 'H') { // Percorre a linha até encontrar o 'H' de "HTTP"
+            if (*ptr == '/') urlStart = ptr; // Marca o início da URL quando encontrar a primeira barra
+            ptr++; // Move o ponteiro para o próximo caractere
+        }
+        ptr--; // Move o ponteiro para o caractere antes do 'H'
+        *ptr = '\0'; //Substitui o espaço antes do "HTTP" por '\0' para terminar a string da URL
+        ht_put(ht, urlStart); // Verifica se a URL já existe na tabela hash e incrementa o contador de hits
+    }
+}
+
+int main(int argc, char* argv[]) {
     HashTable* ht = ht_create(131071); // Cria uma tabela hash com 131.071 buckets
 
-    FILE* file1 = fopen("cdn_data_logs\\manifest.txt", "r"); 
-    
+    if (argc < 3) {
+        fprintf(stderr, "Uso: %s <arquivo_de_urls>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    FILE* file1 = fopen(argv[1], "r");
     if (!file1) {
         perror("Erro ao abrir o arquivo");
         return EXIT_FAILURE;
@@ -20,33 +40,15 @@ int main() {
         ht_put(ht, line); // Adiciona cada linha do arquivo à tabela hash
     }
     
-    FILE* file2 = fopen("cdn_data_logs\\log_concorrente.txt", "r"); 
-    
+    FILE* file2 = fopen(argv[2], "r");
     if (!file2) {
         perror("Erro ao abrir o arquivo");
         return EXIT_FAILURE;
     }
+    
+    ler_log(ht, file2); // Lê o arquivo de log e atualiza a tabela hash
 
-    char line[1024];
-    while (fgets(line, sizeof(line), file2)) {
-        char *ptr = line; // Ponteiro para percorrer a linha
-        char *urlStart = NULL; // Ponteiro para marcar o início da URL
-        
-        while (*ptr != 'H') { // Percorre a linha até encontrar o 'H' de "HTTP"
-            if (*ptr == '/') urlStart = ptr; // Marca o início da URL quando encontrar a primeira barra
-            ptr++; // Move o ponteiro para o próximo caractere
-        }
-        ptr--; // Move o ponteiro para o caractere antes do 'H'
-        *ptr = '\0'; //Substitui o espaço antes do "HTTP" por '\0' para terminar a string da URL
-
-        CacheNode* node = ht_get(ht, urlStart); // Verifica se a URL já existe na tabela hash
-        if (node) {
-            node->hit_count++; // Incrementa o contador de hits se a URL já existir
-        } else {
-            
-            ht_put(ht, urlStart); // Adiciona a URL à tabela hash se não existir
-        }
-    }
+    ht_save_results(ht, "results_seq.csv"); // Salva os resultados em um arquivo CSV
     
     ht_destroy(ht); // Libera a memória
     return 0;
